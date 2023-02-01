a377515aa74df549c819601b44c81ce1de9c9591
dcoeurjo <dcoeurjo@users.noreply.github.com>
2023-02-01 19:30:16 +0000

    Deploying to master from @ DGtal-team/DGtal@02ddc8d26f7b7499f9922a74e1b227fb2a30059f 🚀

---
+++ pr1683/search/pages_b.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['kernel_20concepts_17135',['Kernel Concepts',['../packageKernelConcepts.html',1,'']]],
+  ['kernel_20package_17136',['Kernel package',['../packageKernel.html',1,'']]]
+];
