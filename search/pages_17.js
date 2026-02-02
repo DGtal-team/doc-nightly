3e45e374b13eb00cfba7b9fa96b34d675909146e
dcoeurjo <david.coeurjolly@cnrs.fr>
2026-02-02 08:03:13 +0000

    Deploying to master from @ DGtal-team/DGtal@315e351e4a0ae829b055ffe68507072ed161455f 🚀

---
+++ search/pages_17.js
@@ -0,0 +1,18 @@
+var searchData=
+[
+  ['r3_0',['Surface mesh data structure for representing manifold or non-manifold polygonal surfaces in R3',['../moduleSurfaceMesh.html',1,'packageShapes']]],
+  ['ranges_1',['ranges',['../moduleIteratorsRanges.html',1,'Iterators and ranges'],['../tutoFileGridCurveRanges.html',1,'Tutorial &quot;File -&gt; Grid Curve -&gt; Ranges&quot;']]],
+  ['recognition_20algorithm_2',['Detailed explanation of COBA plane recognition algorithm',['../moduleCOBANaivePlaneRecognition.html',1,'packageGeometry']]],
+  ['recognition_20and_20plane_20width_20computation_3',['Plane recognition and plane width computation',['../modulePlaneRecognition.html',1,'packageGeometry']]],
+  ['recognition_20br_4',['Alpha-thick Segment Recognition   &lt;br&gt;',['../moduleAlphaThickSegmentReco.html',1,'packageGeometry']]],
+  ['referencing_5',['Parameter passing, cloning and referencing',['../moduleCloneAndReference.html',1,'packageBase']]],
+  ['region_20distance_20transformation_6',['Tutorial &quot;Image -&gt; Region -&gt; Distance Transformation&quot;',['../tutoImageRegionDT.html',1,'packageTutorials']]],
+  ['region_20grid_20curve_20extracting_20level_20set_20boundary_20from_20image_7',['Tutorial &quot;Image -&gt; Region -&gt; Grid curve -&gt;  Extracting level set boundary from Image&quot;',['../tutoImageGridCurveExtraction.html',1,'packageTutorials']]],
+  ['region_20grid_20curve_20length_20estimation_8',['Tutorial &quot;Image -&gt; Region -&gt; Grid curve -&gt; Length estimation&quot;',['../tutoImageGridCurveEstimator.html',1,'packageTutorials']]],
+  ['region_20volume_203dvolviewer_20_3a_20a_20simple_203d_20volume_20viewer_20in_20less_20than_2050_20lines_9',['Tutorial &quot;Image -&gt; Region -&gt; Volume -&gt;  3dVolViewer : a simple 3D volume viewer in less than 50 lines&quot;',['../tutoImageGridVolume3dVolViewer.html',1,'packageTutorials']]],
+  ['regularization_20by_20normal_20vector_20alignment_10',['Digital surface regularization by normal vector alignment',['../moduleRegularization.html',1,'packageGeometry']]],
+  ['regularization_20using_20the_20shrouds_20algorithm_11',['Digital surface regularization using the Shrouds algorithm',['../moduleShrouds.html',1,'packageGeometry']]],
+  ['related_20concepts_12',['Metric Spaces, Digital Metric Spaces and Related Concepts',['../moduleMetrics.html',1,'packageGeometry']]],
+  ['relative_20fully_20convex_20envelope_20and_20digital_20polyhedra_13',['Fully convex envelope, relative fully convex envelope and digital polyhedra',['../moduleEnvelope.html',1,'packageGeometry']]],
+  ['representing_20manifold_20or_20non_20manifold_20polygonal_20surfaces_20in_20r3_14',['Surface mesh data structure for representing manifold or non-manifold polygonal surfaces in R3',['../moduleSurfaceMesh.html',1,'packageShapes']]]
+];
---
+++ search/pages_1a.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['unit_20test_20in_20dgtal_20using_20catch_0',['Unit test in DGtal using Catch',['../moduleCatch.html',1,'packageDev']]],
+  ['unoriented_20and_20oriented_20cells_20incidence_1',['Cellular grid space and topology, unoriented and oriented cells, incidence',['../moduleCellularTopology.html',1,'packageTopology']]],
+  ['use_20dgtal_20in_20my_20own_20project_2',['How to use DGtal in my own project ?',['../moduleHowToUseDGtal.html',1,'packageIntroduction']]],
+  ['using_20a_20discrete_20calculus_20model_20of_20ambrosio_20tortorelli_20functional_3',['Piecewise-smooth approximation using a discrete calculus model of Ambrosio-Tortorelli functional',['../moduleGenericAT.html',1,'packageDEC']]],
+  ['using_20catch_4',['Unit test in DGtal using Catch',['../moduleCatch.html',1,'packageDev']]],
+  ['using_20discrete_20polygonal_20calculus_5',['using discrete polygonal calculus',['../moduleGeodesicsInHeat.html',1,'Geodesics In Heat using discrete polygonal calculus'],['../moduleVectorsInHeat.html',1,'Vector Heat Method using discrete polygonal calculus']]],
+  ['using_20functions_20functors_20and_20lambdas_20in_20dgtal_6',['Using functions, functors and lambdas in DGtal',['../moduleFunctors.html',1,'packageBase']]],
+  ['using_20separable_20processes_7',['nD Volumetric Analysis using Separable Processes',['../moduleVolumetric.html',1,'packageGeometry']]],
+  ['using_20the_20shrouds_20algorithm_8',['Digital surface regularization using the Shrouds algorithm',['../moduleShrouds.html',1,'packageGeometry']]]
+];
